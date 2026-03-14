1ff31fbf9d0038a27c48a39c192d5faff80ca4ed
Ghani <3331220055@untirta.ac.id>
2026-03-15 05:01:37 +0700

    Add ModelViewer2D component for 2D visualization

---
+++ frontend/src/components/ModelViewer2D.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ModelViewer2D: React.FC = () => {
+    return (
+        <div>
+            <h1>2D Model Viewer</h1>
+            {/* Implement 2D visualization logic here */}
+        </div>
+    );
+};
+
+export default ModelViewer2D;
