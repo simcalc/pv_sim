c2c0c14595e7cb555d59c1aad5399cdbc577a7b9
Ghani <3331220055@untirta.ac.id>
2026-03-15 05:01:19 +0700

    Create ModelViewer3D component for 3D visualization

---
+++ frontend/src/components/ModelViewer3D.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ModelViewer3D: React.FC = () => {
+    return (
+        <div>
+            {/* 3D Model Visualization Logic */}
+            <h1>3D Model Viewer</h1>
+        </div>
+    );
+};
+
+export default ModelViewer3D;
